d947ba5fb08c2212285f2e04d811a747bc1625ce
Hector <hectorvallsmira2003@gmail.com>
2023-12-15 19:53:28 +0100

    Despliegue funciona

--- deploy.php
+++ deploy.php
@@ -15,6 +15,7 @@ add('writable_dirs', []);
 
 host('34.227.81.64')
     ->set('remote_user', 'prod-ud4-deployer')
+    ->set('ssh_multiplexing', false)
     ->set('identityFile', '..\\..\\DDAW-KEY-PAIRUD2-hector-valls-mira.pem')
     ->set('deploy_path', '/var/www/prod-ud4-a4/html/todo-laravel');
 
@@ -22,8 +23,14 @@ host('34.227.81.64')
 
 after('deploy:failed', 'deploy:unlock');
 
-
 task('upload:env', function () {
     upload('.env.production', '{{deploy_path}}/shared/.env');
-   })->desc('Environment setup');
-   
+})->desc('Environment setup');
+      
+
+# Declaració de la tasca
+task('reload:php-fpm', function () {
+    run('sudo /etc/init.d/php8.1-fpm restart');
+   });
+   # inclusió en el cicle de desplegament
+   after('deploy', 'reload:php-fpm');
